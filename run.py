595b32e351f427c81bdc651e1746f84145f87eb9
JSonliuJ <492224300@qq.com>
2022-04-17 16:10:46 +0800

    run.py

---
+++ run.py
@@ -0,0 +1,7 @@
+# -- encoding: utf-8 --
+# @time:    	2022/4/17 12:03
+# @Author: 		JsonLiu
+# @Email:  		492224300@qq.com
+import pytest
+pytest.main(["-v","-s","-m","TestDemo02"])
+# pytest.main() # 收集到的用例，全部运行
