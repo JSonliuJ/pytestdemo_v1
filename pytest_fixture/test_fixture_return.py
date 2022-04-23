7d0ea295434328e76a45735f058066ae25a3bbfe
JSonliuJ <492224300@qq.com>
2022-04-24 00:35:03 +0800

    test_fixture_return.py

--- pytest_fixture/test_fixture_return.py
+++ pytest_fixture/test_fixture_return.py
@@ -3,7 +3,7 @@
 # @Author: 		JsonLiu
 # @Email:  		492224300@qq.com
 import pytest
-
+import time
 
 @pytest.fixture(scope='class')
 def mysql_connect():
@@ -21,6 +21,8 @@ class TestReturnDemo001():
 
     def test_baidu001(self,driver_init): # driver_init = (driver,True)
         driver_init[0].find_element_by_id("kw").send_keys("pytest")
+        driver_init[0].find_element_by_xpath('//input[@id="su"]').click()
+        time.sleep(10)
 
 
 # 方式1：@pytest.mark.usefixtures('定义的fixture函数')
